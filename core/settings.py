1f48f51abd3f14373c95522e8cac1abbdf0e2b27
mregorlite <egor.infinity@gmail.com>
2025-01-04 11:19:15 +0200

    change SECRET_KEY from env and add health checker

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-h$olrv^cu(ge*&gjsut1x9u_-f37y%da=k-64uy%hn@k4f-j!z"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
